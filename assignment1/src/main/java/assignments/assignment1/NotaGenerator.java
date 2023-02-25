ac78e2204540a5f5fef6261787d2694dc033484a
hilmiatha <hilmiatha2003@gmail.com>
2023-02-25 23:46:11 +0700

    fixed generateId function

--- assignment1/src/main/java/assignments/assignment1/NotaGenerator.java
+++ assignment1/src/main/java/assignments/assignment1/NotaGenerator.java
@@ -10,6 +10,75 @@ public class NotaGenerator {
      */
     public static void main(String[] args) {
         // TODO: Implement interface menu utama
+        boolean state = true;
+        while (state){
+            printMenu();
+            System.out.print("Pilihan : ");
+            int pilihanUser;
+            try {
+                pilihanUser = Integer.parseInt(input.nextLine());
+            }catch(NumberFormatException e){
+                System.out.println("Perintah tidak diketahui, silakan periksa kembali.");
+                System.out.println();
+                continue;
+            }
+            if (pilihanUser==0){
+                System.out.println("Terima kasih telah menggunakan NotaGenerator!");
+                state = false;
+                input.close();
+            } else if (pilihanUser==1) {
+                System.out.println("Masukkan nama Anda:");
+                String namaUser = input.next();
+                input.nextLine();
+                System.out.println("Masukkan nomor handphone Anda:");
+                String nomorHp;
+                while (true) {
+                    nomorHp = input.nextLine();
+                    if (nomorHp.chars().allMatch(Character::isDigit)){
+                        break;
+                    }else {
+                        System.out.println("Nomor hp hanya menerima digit");
+                    }
+                }
+                System.out.println("ID Anda : " + generateId(namaUser,nomorHp));
+                System.out.println();
+
+            } else if (pilihanUser==2) {
+                System.out.println("Masukkan nama Anda:");
+                String namaUser = input.next();
+                input.nextLine();
+                System.out.println("Masukkan nomor handphone Anda:");
+                String nomorHp;
+                while (true) {
+                    nomorHp = input.nextLine();
+                    if (nomorHp.chars().allMatch(Character::isDigit)) {
+                        break;
+                    } else {
+                        System.out.println("Nomor hp hanya menerima digit");
+                    }
+                }
+                System.out.println("Masukkan tanggal terima :");
+                String tanggalTerima = input.nextLine();
+                System.out.println("Masukkan paket laundry :");
+                String paketLaundry = input.nextLine();
+                while (true) {
+                    if (paketLaundry.equalsIgnoreCase("express")| paketLaundry.equalsIgnoreCase("fast")| paketLaundry.equalsIgnoreCase("reguler")){
+                        System.out.println();
+                        break;
+                    } else if (paketLaundry.equals("?")) {
+                        showPaket();
+                        System.out.println("Masukkan paket laundry :");
+                        paketLaundry = input.nextLine();
+                        System.out.println();
+                    }
+
+
+                }
+            }else{
+                System.out.println("Perintah tidak diketahui, silakan periksa kembali.");
+                System.out.println();
+            }
+        }
     }
 
     /**
@@ -42,8 +111,37 @@ public class NotaGenerator {
      */
     public static String generateId(String nama, String nomorHP){
         // TODO: Implement generate ID sesuai soal.
-        return null;
-    }
+        String upperCaseName = nama.toUpperCase();
+        String namaDanNomor = upperCaseName + "-" + nomorHP;
+        int angkaChecksum = 0; //starting point untuk menghitung angka checksum
+        for (int i = 0; i<namaDanNomor.length();i++){
+            char c = namaDanNomor.charAt(i);
+            if (Character.isLetter(c)){
+                angkaChecksum += c - 'A' + 1;
+            }else if(Character.isDigit(c)){
+                int digit = Character.digit(c, 10);
+                angkaChecksum += digit;
+            }else{
+                angkaChecksum += 7;
+            }
+        }
+        String output;
+        String checksumStr;
+        if (angkaChecksum < 10){
+            checksumStr = "0" + angkaChecksum;
+            output = namaDanNomor + "-" + checksumStr;
+        }else if(angkaChecksum > 100){
+            checksumStr = Integer.toString(angkaChecksum).substring(Integer.toString(angkaChecksum).length()-2);//conditionals apabila checksum lebih dari 2 digit
+            output = namaDanNomor + "-" + checksumStr;
+        }else{
+            output = namaDanNomor + "-" + angkaChecksum;
+        }
+        return output;
+
+        }
+//
+
+
 
     /**
      *
